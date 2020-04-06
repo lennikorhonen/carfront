76a9e564566ba18fdcaa081d242c2721fcec2597
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-04-06 14:02:29 +0300

    New functionalities

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -3,10 +3,13 @@ import ReactTable from 'react-table-v6'
 import 'react-table-v6/react-table.css'
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Addcar from './Addcar';
+import Editcar from './Editcar';
 
 export default function Carlist() {
     const [cars, setCars] = useState([]);
     const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
         getCars();
@@ -23,9 +26,43 @@ export default function Carlist() {
         if (window.confirm('Are you sure?')){
         fetch(link, {method: 'DELETE'})
         .then(_ => getCars())
-        .then(_ => setOpen(true))
+        .then(_ => {
+            setMsg('Car deleted')
+            setOpen(true)
+        })
         .catch(err => console.error(err))
         }
+    }
+
+    const addCar = (car) => {
+        fetch('https://carstockrest.herokuapp.com/cars',
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(car)
+        }
+        )
+        .then(_ => getCars())
+        .then(_ => {
+            setMsg('New car added');
+            setOpen(true);
+        })
+        .catch(err => console.error())
+    }
+
+    const updateCar = (link, car) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(car)
+        }
+        )
+        .then(_ => getCars())
+        .then(_ => {
+            setMsg('Car updated');
+            setOpen(true);
+        })
+        .catch(err => console.error())
     }
 
     const handleClose = () => {
@@ -58,18 +95,23 @@ export default function Carlist() {
             accessor: 'price'
         },
         {
+            Cell: row => (<Editcar car={row.original} updateCar={updateCar} />)
+        },
+        {
             Cell: row => (<Button size='small' color='secondary' onClick={() => deleteCar(row.original._links.self.href)} >Delete</Button>)
         }
+        
     ]
 
     return(
     <div>
+        <Addcar addCar={addCar}/>
         <ReactTable defaultPageSize={10} filterable={true} data={cars} columns={columns} />
         <Snackbar
             open={open}
             autoHideDuration={3000}
             onClose={handleClose}
-            message='Car deleted'
+            message={msg}
             anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'left',
